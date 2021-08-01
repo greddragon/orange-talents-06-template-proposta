3d7b511c5d9cc78c6dd850d34b241f164f68ffc2
gerson dos santos de souza <gersonsouza97@outlook.com>
2021-07-31 23:53:09 -0300

    setup projeto proposta

---
+++ src/main/java/br/com/academy/gerson/projetoproposta/ProjetoPropostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.academy.gerson.projetoproposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPropostaApplication.class, args);
+	}
+
+}
